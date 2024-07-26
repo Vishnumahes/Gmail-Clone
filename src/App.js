ce732deabf72065442b495a8b018b23f94eec1c3
Vishnu Vardhan Maheswaran <vishnu191299@gmail.com>
2024-07-25 23:14:09 -0700

    initial set of header and css of it over sidebar is started

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './App.css';
 import Header from "./Header";
+import Sidebar from './Sidebar';
+
+
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Sidebar/>
       <h1>Lets build Gmail</h1>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,74 @@
-import React from 'react'
+// import React from 'react';
+// import './Header.css';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import { IconButton } from '@material-ui/core';
+// import SearchIcon from '@mui/icons-material/Search';
+// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+// import AppsIcon from "@mui/icons-material/Apps";
+// import NotificationsIcon from "@mui/icons-material/Notifications";
+
+// function Header() {
+//   return (
+//     <div className='header'>
+//         <div className='header__left'>
+//             <MenuIcon />
+//             <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="Gmail Logo"/>
+//         </div>
+//         <div className='header__middle'>
+//             <SearchIcon />
+//             <input placeholder="Search mail" type="text" />
+//             <ArrowDropDownIcon className="header__inputCaret"/>
+//         </div>
+//         <div className='header__right'>
+//             <IconButton>
+//                 <AppsIcon />
+//             </IconButton>
+//             <IconButton>
+//                 <NotificationsIcon />
+//             </IconButton>
+//         </div>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+import React from 'react';
 import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from '@material-ui/core';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Avatar from '@mui/material/Avatar';
 function Header() {
   return (
-    <div className='Header'>
+    <div className='header'>
         <div className='header__left'>
-        <MenuIcon />
-        <IconButton/> 
-
-        
-              <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"alt=""/>
+            <IconButton>
+                <MenuIcon />
+            </IconButton>
+            <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="Gmail Logo"/>
         </div>
         <div className='header__middle'>
-
+            <SearchIcon />
+            <input placeholder="Search mail" type="text" />
+            <ArrowDropDownIcon className="header__inputCaret"/>
         </div>
         <div className='header__right'>
-
+            <IconButton>
+                <AppsIcon />
+            </IconButton>
+            <IconButton>
+                <NotificationsIcon />
+            </IconButton>
+            <IconButton>
+                <Avatar />
+            </IconButton>
         </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
